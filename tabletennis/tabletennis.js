a9446e23f57915477ba98d2de03eddd17dc9129c
jbx-coder <jbxyyy@outlook.com>
2025-05-03 13:46:11 +0800

    Add files via upload

---
+++ tabletennis/tabletennis.js
@@ -0,0 +1,82 @@
+var point1=0
+var point2=0
+var score1=0
+var score2=0
+var redpoint =document.getElementById('redpoint')
+var red =document.getElementById('red')
+var bluepoint =document.getElementById('bluepoint')
+var blue =document.getElementById('blue')
+var clear=document.getElementById('clear')
+getdata()
+
+function getten(a){
+    if (a>9){
+        return a
+    }else{
+        return '0'+a
+    }
+}
+function judge(){
+    if(point1 >=11 && point1-point2>1){
+        score1+=1
+        bluepoint.innerHTML=score1
+        point1=0
+        point2=0
+        red.innerHTML=getten(point2)
+        blue.innerHTML=getten(point1)
+    }else if(point2 >=11 && point2-point1>1){
+        score2+=1
+        redpoint.innerHTML=score2
+        point1=0
+        point2=0
+        red.innerHTML=getten(point2)
+        blue.innerHTML=getten(point1)
+    }else{
+        return
+    }
+}
+function setdata(){
+    data=[
+        point1,
+        point2,
+        score1,
+        score2
+    ]
+    localStorage.setItem('jbx-coder-sportscounter-tabletennis',JSON.stringify(data))
+}
+function getdata(){
+    if('jbx-coder-sportscounter-tabletennis' in localStorage){
+        data=JSON.parse(localStorage.getItem('jbx-coder-sportscounter-tabletennis'))
+        point1=data[0]
+        point2=data[1]
+        score1=data[2]
+        score2=data[3]
+        red.innerHTML=getten(point2)
+        blue.innerHTML=getten(point1)
+        redpoint.innerHTML=score2
+        bluepoint.innerHTML=score1
+    }
+}
+clear.onclick=function(){
+    point1=0
+    point2=0
+    score1=0
+    score2=0
+    red.innerHTML=getten(point2)
+    blue.innerHTML=getten(point1)
+    redpoint.innerHTML=score2
+    bluepoint.innerHTML=score1
+    setdata()
+}
+red.onclick=function(){
+    point2+=1
+    red.innerHTML=getten(point2)
+    judge()
+    setdata()
+}
+blue.onclick=function(){
+    point1+=1
+    blue.innerHTML=getten(point1)
+    judge()
+    setdata()
+}
